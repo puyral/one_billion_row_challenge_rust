6b270b1f703bd1a6e9c1afb3ca1f635e900bb98f
puyral <7871851+puyral@users.noreply.github.com>
2025-12-04 17:46:10 +0100

    fast

--- src/parser.rs
+++ src/parser.rs
@@ -1,10 +1,12 @@
 use std::{
     hint::unreachable_unchecked,
+    os::unix::raw::off_t,
     simd::{
         Mask, Simd, i16x4,
         prelude::{SimdInt, SimdPartialEq, SimdUint},
         u8x4, u8x8, u8x16, u8x32, u16x4, u64x4, usizex4,
     },
+    usize,
 };
 
 use crate::fsize;
@@ -40,6 +42,7 @@ impl<'a> Iterator for Finder<'a> {
         let temperature_end_idx = *current + temperature_end_idx;
 
         let station = &data[*current..station_end_idx];
+
         let temperature_idx = station_end_idx + 1;
         let temperature = parse_value(data, temperature_idx, temperature_end_idx);
 
@@ -49,7 +52,7 @@ impl<'a> Iterator for Finder<'a> {
 }
 
 #[allow(nonstandard_style)]
-type u8xx = u8x16;
+type u8xx = u8x32;
 static NUMER_SKIPPED: usize = u8xx::LEN;
 
 /// repeats `e` for the while number
@@ -59,16 +62,87 @@ macro_rules! mk_splat {
     };
 }
 
+static SWAR_STATION: bool = false;
+
 #[inline(never)]
 fn find_next(data: &[u8]) -> Option<(usize, usize)> {
-    match sawr_search(data) {
-        Some(idsc) => Some((idsc, find_temperature(&data[idsc + 1..]) + idsc + 1)),
-        None if data.len() >= NUMER_SKIPPED => slow_search(data),
-        _ => None,
+    if SWAR_STATION {
+        // deactivated
+        match sawr_station_search(data) {
+            Some(idsc) => Some((idsc, find_temperature(data, idsc))),
+            None if data.len() > NUMER_SKIPPED => slow_search(data, NUMER_SKIPPED),
+            _ => None,
+        }
+    } else {
+        #[allow(clippy::collapsible_else_if)]
+        if data.len() < MIN_SIMD_LEN {
+            // rare slow path
+            let idsc = data.iter().position(|x| *x == b';')?;
+            Some((idsc, find_temperature(data, idsc)))
+        } else {
+            simd_search(data)
+        }
     }
 }
 
-fn sawr_search(data: &[u8]) -> Option<usize> {
+static MIN_SIMD_LEN: usize = 100_usize.div_ceil(u8xx::LEN) * u8xx::LEN;
+fn simd_search(data: &[u8]) -> Option<(usize, usize)> {
+    assert!(data.len() >= MIN_SIMD_LEN);
+    let upper = MIN_SIMD_LEN / u8xx::LEN;
+    let delimiter_nl = u8xx::splat(b'\n');
+    let delimiter_sc = u8xx::splat(b';');
+
+    for i in 0..upper {
+        let offset = i * u8xx::LEN;
+        // because of the assertion
+        let line = unsafe {
+            u8xx::load_select_unchecked(&data[offset..], Mask::splat(true), u8xx::splat(0))
+        };
+
+        let sc = delimiter_sc.simd_eq(line).first_set();
+        let nl = delimiter_nl.simd_eq(line).first_set();
+
+        match (sc, nl) {
+            (Some(idsc), Some(idnl)) => return Some((offset + idsc, offset + idnl)),
+            (Some(idsc), None) => {
+                return Some((idsc, find_temperature(data, offset + idsc)));
+            }
+            (None, None) => continue,
+            _ => unsafe { unreachable_unchecked() },
+        }
+    }
+    None
+}
+
+fn slow_search(data: &[u8], skipped: usize) -> Option<(usize, usize)> {
+    let idsc = memchr::memchr(b';', &data[skipped - 1..])? + skipped - 1;
+    let idnl = find_temperature(data, idsc);
+    Some((idsc, idnl))
+}
+
+fn find_temperature(data: &[u8], offset: usize) -> usize {
+    let offset = offset + 1;
+    let data = &data[offset..];
+
+    let res = if data.len() < 8 {
+        // nearly never run
+        data.iter().position(|x| *x == b'\n').unwrap()
+    } else {
+        let data: &[u8] = data;
+        debug_assert!(data.len() >= 8);
+        // SAFETY: We assume the buffer has at least 8 bytes available
+        let chunk = unsafe { (data.as_ptr() as *const u64).read_unaligned() };
+        let pattern = mk_splat!(u64; b'\n');
+        let xored = chunk ^ pattern;
+        let low_magic = mk_splat!(u64; 0x01);
+        let high_magic = mk_splat!(u64; 0x80);
+        let mask = (xored.wrapping_sub(low_magic)) & !xored & high_magic;
+        (mask.trailing_zeros() >> 3) as usize
+    };
+    res + offset
+}
+
+fn sawr_station_search(data: &[u8]) -> Option<usize> {
     assert_eq!(NUMER_SKIPPED, ::std::mem::size_of::<u128>());
     if data.len() < NUMER_SKIPPED {
         // slow final path
@@ -91,65 +165,6 @@ fn sawr_search(data: &[u8]) -> Option<usize> {
     } else {
         Some((mask.trailing_zeros() / 8) as usize)
     }
-}
-
-fn slow_search(data: &[u8]) -> Option<(usize, usize)> {
-    let idsc = memchr::memchr(b';', &data[NUMER_SKIPPED - 1..])? + NUMER_SKIPPED - 1;
-    let idnl = find_temperature(&data[idsc + 1..]) + idsc + 1;
-    Some((idsc, idnl))
-}
-
-fn find_station_slow(data: &[u8]) -> Option<usize> {
-    if data.len() >= NUMER_SKIPPED {
-        Some(memchr::memchr(b';', &data[NUMER_SKIPPED - 1..])? + NUMER_SKIPPED - 1)
-    } else {
-        None
-    }
-}
-
-fn find_temperature(data: &[u8]) -> usize {
-    if data.len() < 8 {
-        // SIMD
-        let delimiter = u8x8::splat(b'\n'); // 5 max
-        let line = u8x8::load_or_default(data);
-        let delimeq = delimiter.simd_eq(line);
-        // We know
-        unsafe { delimeq.first_set().unwrap_unchecked() }
-    } else {
-        let data: &[u8] = data;
-        debug_assert!(data.len() >= 8);
-        // SAFETY: We assume the buffer has at least 8 bytes available
-        let chunk = unsafe { (data.as_ptr() as *const u64).read_unaligned() };
-
-        // 2. XOR: "Subtract" the newline char.
-        let pattern = mk_splat!(u64; b'\n');
-        let xored = chunk ^ pattern;
-
-        // 3. The "Has Zero Byte" Trick
-        // (v - 0x01) & !v & 0x80 detects zero bytes.
-        let low_magic = mk_splat!(u64; 0x01);
-        let high_magic = mk_splat!(u64; 0x80);
-
-        // This results in 0x80 in the byte slot where \n was, and 0x00 elsewhere.
-        let mask = (xored.wrapping_sub(low_magic)) & !xored & high_magic;
-
-        // 4. Convert bit index to byte index.
-        // trailing_zeros gives bits (e.g., 0, 8, 16...).
-        // Divide by 8 (shift right 3) to get byte index (0, 1, 2...).
-        // SAFETY: We assume a newline exists, otherwise `mask` is 0 and result is garbage (8).
-        (mask.trailing_zeros() >> 3) as usize
-    }
-}
-
-fn simd_search(data: &[u8]) -> (Option<usize>, Option<usize>) {
-    // if SIMD {
-    let delimiter_nl = u8xx::splat(b'\n');
-    let delimiter_sc = u8xx::splat(b';');
-    let line = u8xx::load_or_default(data);
-
-    let sc = delimiter_sc.simd_eq(line).first_set();
-    let nl = delimiter_nl.simd_eq(line).first_set();
-    (sc, nl)
 }
 
 fn compute_shape(str: &[u8], start: usize, end: usize) -> (bool, bool) {
